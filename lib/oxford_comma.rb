ff0327d4dc09f3022e03997d8ae00b47a0ef6e3d
Ben Jay <fakebenjay@gmail.com>
2017-01-23 20:45:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+array = ["fiddleheads","okra","kohlrabi"]
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array[-1].prepend("and ")
+    array.join(", ")
+  end
 end
